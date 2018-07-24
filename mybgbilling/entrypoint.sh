6a9537459b17c7c252d45b52eab65f69ece9ef25
amir <amir@bitel.ru>
2018-07-24 17:30:30 +0500

    mybgbilling

---
+++ mybgbilling/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function sigterm_handler() {
+    echo "SIGTERM signal received"
+}
+
+trap "sigterm_handler; exit" TERM
+
+function entrypoint() {
+
+    echo "entrypoint.sh"
+
+    if [ -z "$DEPLOY_WAIT_FOR_SERVER" ]; then
+      DEPLOY_WAIT_FOR_SERVER="server:8080 -t 240"
+    fi
+
+    # ожидаем старта BGBillingServer, т.к. в при deploy может происходить установка модулей
+    echo "Waiting for SERVER (${DEPLOY_WAIT_FOR_SERVER})..."
+    /wait-for.sh $DEPLOY_WAIT_FOR_SERVER
+
+    $JBOSS_HOME/bin/mybgbilling-sync-libs.sh
+
+    echo "Starting Wildfly"
+
+    exec "$@"
+}
+
+entrypoint "$@"
