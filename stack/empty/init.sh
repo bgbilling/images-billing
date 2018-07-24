695868f116fa0ba9981b1333bafd9d1c10d315a4
amir <amir@bitel.ru>
2018-07-24 13:20:49 +0500

    stack-empty: fix init.sh

--- stack/empty/init.sh
+++ stack/empty/init.sh
@@ -11,8 +11,10 @@ docker pull mysql/mysql-server:5.7
 echo "Init Docker Swarm"
 docker swarm init
 
+stackDir=$(dirname "$0")
+
 echo "Deploing bgbilling-docker"
-docker stack deploy -c ./bgbilling-docker/docker-stack.yml bgbilling
+docker stack deploy -c ${stackDir}/docker-stack.yml bgbilling
 
 # show logs
 timeout 180 docker service logs bgbilling_db --follow & timeout 180 docker service logs bgbilling_server --follow || docker ps
