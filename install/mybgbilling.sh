12722b31aa8b4b97e133dcd040b2d6c2e67cd548
amir <amir@bitel.ru>
2018-10-10 20:00:32 +0500

    change setenv

--- install/mybgbilling.sh
+++ install/mybgbilling.sh
@@ -21,9 +21,13 @@ set -x \
   && sed -i "s@user = 'customer'@user = 'admin'@" /tmp/bgb-install/MyBGBilling.war/WEB-INF/mybgbilling-conf.groovy \
   && sed -i "s@password = '123456'@password = 'admin'@" /tmp/bgb-install/MyBGBilling.war/WEB-INF/mybgbilling-conf.groovy \
   \
+  && { \
+    echo '#!/bin/sh'; \
+    echo; \
+    echo '#JAVA_HOME='; \
+    echo 'MYBGBILLING_HOME=/opt/wildfly/current/standalone/deployments/MyBGBilling.war'; \
+  } >> /tmp/bgb-install/MyBGBilling.war/WEB-INF/script/files/setenv.sh \
   && chmod +x /tmp/bgb-install/MyBGBilling.war/WEB-INF/script/files/*.sh \
-  && sed -i 's@JAVA_HOME=@#JAVA_HOME=@' /tmp/bgb-install/MyBGBilling.war/WEB-INF/script/files/setenv.sh \
-  && sed -i 's@MYBGBILLING_HOME=@MYBGBILLING_HOME=/opt/wildfly/current/standalone/deployments/MyBGBilling.war@' /tmp/bgb-install/MyBGBilling.war/WEB-INF/script/files/setenv.sh \
   && cp /tmp/bgb-install/MyBGBilling.war/WEB-INF/script/files/*.* $WILDFLY_HOME/bin \
   \
   && echo "Starting Wildfly" \
