a796446ce73f6ddd0b1dd967581d1013b62a57d2
amir <amir@bitel.ru>
2018-10-10 19:35:11 +0500

    fix path

--- install/mybgbilling.sh
+++ install/mybgbilling.sh
@@ -22,9 +22,9 @@ set -x \
   && sed -i "s@password = '123456'@password = 'admin'@" /tmp/bgb-install/MyBGBilling.war/WEB-INF/mybgbilling-conf.groovy \
   \
   && chmod +x /tmp/bgb-install/MyBGBilling.war/WEB-INF/script/files/*.sh \
-  && cp /tmp/bgb-install/MyBGBilling.war/WEB-INF/script/files/*.* $WILDFLY_HOME/bin \
   && sed -i 's@JAVA_HOME=@#JAVA_HOME=@' /tmp/bgb-install/MyBGBilling.war/WEB-INF/script/files/setenv.sh \
   && sed -i 's@MYBGBILLING_HOME=@MYBGBILLING_HOME=/opt/wildfly/current/standalone/deployments/MyBGBilling.war@' /tmp/bgb-install/MyBGBilling.war/WEB-INF/script/files/setenv.sh \
+  && cp /tmp/bgb-install/MyBGBilling.war/WEB-INF/script/files/*.* $WILDFLY_HOME/bin \
   \
   && echo "Starting Wildfly" \
   && systemctl start wildfly \
